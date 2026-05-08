60ddb695f02b7db6901eee59d815795e66d886f5
GERMAN <german.amiquero.27@unsch.edu.pe>
2026-05-08 16:02:55 -0500

    primewr commit

--- tests/test_saludo.py
+++ tests/test_saludo.py
@@ -1,6 +1,6 @@
-def saludar(nombre):
-    return f"Hola {nombre}"
+def saludar(nombres):
+    return f"Hola {nombres}"
 
 
 def test_saludar():
-    assert saludar("Ana") == "Hola Ana"
+    assert saludar("Ana") == "Hola Ana"!
