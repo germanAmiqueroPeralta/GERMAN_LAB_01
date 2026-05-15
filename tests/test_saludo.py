2bd8d7b5e87cb0b0bf9edc6a56af6eab6c65e7d8
germanAmiqueroPeralta <german.amiquero.27@unsch.edu.pe>
2026-05-15 10:12:37 -0500

    nuevo arreglo

--- tests/test_saludo.py
+++ tests/test_saludo.py
@@ -2,5 +2,5 @@ def saludar(nombres):
     return f"Hola {nombres}"
 
 
-def test_saludar():
-    assert saludar("Ana") == "Hola Ana"
+def test_saludar_01():
+    assert saludar("Ana") == "Hola Ana soy yo desde mi laptop"
